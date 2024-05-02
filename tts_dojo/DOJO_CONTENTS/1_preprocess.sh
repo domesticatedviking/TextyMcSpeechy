3dc7bd51d4450ef18f3631f3841714ad9935ce1e
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2024-05-02 10:19:53 -0700

    fixed path

--- tts_dojo/DOJO_CONTENTS/1_preprocess.sh
+++ tts_dojo/DOJO_CONTENTS/1_preprocess.sh
@@ -1,7 +1,7 @@
 python3 -m piper_train.preprocess \
   --language en-us \
   --input-dir ./target_voice_dataset \
-  --output-dir ./training_directory \
+  --output-dir ./training_folder \
   --dataset-format ljspeech \
   --single-speaker \
   --sample-rate 16000
