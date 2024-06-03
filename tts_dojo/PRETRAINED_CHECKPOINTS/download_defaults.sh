e44b77a5c16323b9f418c396f5870418ce895263
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2024-06-03 12:14:41 -0700

    Added framework for multi language support

--- tts_dojo/PRETRAINED_CHECKPOINTS/download_defaults.sh
+++ tts_dojo/PRETRAINED_CHECKPOINTS/download_defaults.sh
@@ -2,27 +2,78 @@
 
 # URL downloader for pretrained piper checkpoint files
 # Feel free to replace the links below with your preferred languages and voices, or manually download and copy these files into their respective folders.
-# Please be aware that all quality levels are not available for all voices, and there are no en_US options for low quality feminine voices.
 
-# You will find all available options here:  https://huggingface.co/datasets/rhasspy/piper-checkpoints/tree/main
- 
+# A good place to find pretrained checkpoints is:  https://huggingface.co/datasets/rhasspy/piper-checkpoints/tree/main
 
-# Low, medium, and high quality piper checkpoints for traditionally masculine "lessac" voice (en_US)
-                   
-DEFAULT_M_LOW_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/lessac/low/epoch%3D2307-step%3D558536.ckpt?download=true"
-DEFAULT_M_MED_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/lessac/medium/epoch%3D2164-step%3D1355540.ckpt?download=true"
-DEFAULT_M_HIGH_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/lessac/high/epoch%3D2218-step%3D838782.ckpt?download=true"
+set +e  # terminate on error
 
-# Low, medium, and high quality piper checkpoints for tradionally feminine voice (en_US)
-DEFAULT_F_LOW_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/lessac/low/epoch%3D2307-step%3D558536.ckpt?download=true"  # sadly no pretrained "low" models for F voices are available.
-DEFAULT_F_MED_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/amy/medium/epoch%3D6679-step%3D1554200.ckpt?download=true"  # amy_medium
-DEFAULT_F_HIGH_URL="https://huggingface.co/datasets/rhasspy/piper-checkpoints/resolve/main/en/en_US/ljspeech/high/ljspeech-2000.ckpt?download=true" #ljspeech high
+# Directory containing the language files
+LANG_DIR="languages"
+
+# Array to store language filenames without extensions
+languages=()
 
 
 
+check_language() {
+    local language_string="$1"
+    local found=0
 
-# Code below dowloads the links above into the appropriate folders.
+    for language in "${languages[@]}"; do
+        if [[ "$language" == "$language_string" ]]; then
+            found=1
+            break
+        fi
+    done
 
+    if [[ ! $found -eq 1 ]]; then
+        echo "Invalid language string:  '$language_string'.  Exiting."
+        exit 1
+    fi
+}
+
+load_language_options(){
+# Read files in the directory and store filenames without extensions in the array
+for file in "$LANG_DIR"/*.conf; do
+    if [[ -f "$file" ]]; then
+        filename=$(basename "$file" .conf)
+        languages+=("$filename")
+    fi
+done
+}
+
+# Output all languages
+print_language_options(){
+for language in "${languages[@]}"; do
+    echo "        $language"
+done
+}
+
+
+# Check if language parameter is passed
+if [ -z "$1" ]; then
+    load_language_options
+    echo "No language specified. Usage: $0 <language>"
+    echo 
+    echo "    available options for <language> are:"
+    print_language_options
+    echo
+    echo "To add a language, use .conf files in the languages directory as a template."
+    echo "Pull requests will be received with gratitude."
+    echo
+    echo "You can also manually add your checkpoint files to the appropriate folders in 'PRETRAINED_CHECKPOINT_FILES/default' if you prefer."
+    exit 1
+fi
+
+# Store the language parameter in requested_language variable
+requested_language="$1"
+load_language_options
+check_language "$requested_language"
+source "./languages/""$requested_language.conf"
+
+
+
+# Code below downloads the links above into the appropriate folders.
 
 M_URLS=( "$DEFAULT_M_LOW_URL" "$DEFAULT_M_MED_URL" "$DEFAULT_M_HIGH_URL")
 F_URLS=( "$DEFAULT_F_LOW_URL" "$DEFAULT_F_MED_URL" "$DEFAULT_F_HIGH_URL")
@@ -36,7 +87,7 @@ get_filename_from_url() {
     # Extract the filename from the URL
     filename=$(basename "$url")
     
-    # strip ?download=true
+    # Strip ?download=true
     filename=$(echo "$filename" | cut -d'?' -f1)
     
     # Decode URL-encoded characters
@@ -45,12 +96,10 @@ get_filename_from_url() {
     echo "$filename"
 }
 
-
 get_quality_from_url() {
     local url="$1"
     local path=""
     local parent_folder=""
-
 
     # Extract the path from the URL by removing the protocol and domain
     path="${url#https://*/}"
@@ -71,22 +120,19 @@ download_urls(){
     local url_array=("${@}") 
     local filename=""
     
-for url in "${url_array[@]}"; do
-    filename=$(get_filename_from_url "$url")
-    quality=$(get_quality_from_url "$url")
+    for url in "${url_array[@]}"; do
+        filename=$(get_filename_from_url "$url")
+        quality=$(get_quality_from_url "$url")
 
-    if [ -e "$PRETRAINED_TTS_DIR/$subfolder/$quality/$filename" ]; then
-        echo -e "\n$subfolder/$quality/$filename already exists.  Not downloading."
-    else
-    echo -e "\nDownloading $filename to $subfolder/$quality."
-        #wget -P "$PRETRAINED_TTS_DIR/$subfolder/$quality/"  $url
-    fi
-done
-
+        if [ -e "$PRETRAINED_TTS_DIR/$subfolder/$quality/$filename" ]; then
+            echo -e "\n$subfolder/$quality/$filename already exists.  Not downloading."
+        else
+            echo -e "\nDownloading $filename to $subfolder/$quality."
+            #wget -P "$PRETRAINED_TTS_DIR/$subfolder/$quality/"  $url
+        fi
+    done
 }
 
-
 download_urls "default/M_voice" "${M_URLS[@]}"
-
 download_urls "default/F_voice" "${F_URLS[@]}"
 
