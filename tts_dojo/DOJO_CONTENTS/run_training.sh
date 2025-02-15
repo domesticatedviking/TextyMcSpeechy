127a5db7018d4d866779959f552df12a7ec6bb85
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2025-02-15 07:56:40 -0800

    check existence of docker files

--- tts_dojo/DOJO_CONTENTS/run_training.sh
+++ tts_dojo/DOJO_CONTENTS/run_training.sh
@@ -35,9 +35,24 @@ else
     exit 1
 fi
 
- 
-echo "Starting Docker container textymcspeechy-piper"
+# Prepare to launch docker container.
+
 cd ../.. #docker-conf.yml is in TextyMcSpeechy dir, 2 levels up from <name>_dojo
+
+# ensure docker files exist.
+if [[ ! -f "docker-compose.yml" ]]; then
+    echo "Error: docker-compose.yml not found! Press <Enter> to exit." >&2
+    read
+    exit 1
+fi
+
+if [[ ! -f "Dockerfile" ]]; then
+    echo "Error: Dockerfile not found! Press <Enter> to exit." >&2
+    read
+    exit 1
+fi
+
+echo "Starting Docker container textymcspeechy-piper"
 # pass PUID and PGID to docker-compose.yml (prepend as environment vars) 
 # Ensures current user will have rights to files created by docker container in mounted folder
 export TMS_USER_ID=$(id -u)
