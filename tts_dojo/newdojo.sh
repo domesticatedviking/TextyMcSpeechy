e94f884a4f6930348a551faf48e7d8ca1c43e18a
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2025-02-15 15:43:22 -0800

    Update newdojo.sh

--- tts_dojo/newdojo.sh
+++ tts_dojo/newdojo.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# newdojo.sh - creates and configures a new dojo for training piper voice models
+
+set -e # abort script on all errors
+
+# color codes
 RESET='\033[0m' # Reset text color to default
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
 
-initial_dir=$(pwd)
-
-# Check if a parameter is provided
+# Check if user provided a name
 if [ -z "$1" ]; then
   echo
   echo 
@@ -20,62 +23,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-
-if [[ ! -e ".TEXTY_DIR" ]]; then
-    echo "Path to TextyMcSpeechy directory not found in '.TEXTY_DIR'"
-    echo "This is normally created by install_piper.sh" 
-    exit 1
-else
-    TEXTY_DIR=$(cat ".TEXTY_DIR")
-fi
-
-if [ ! -e ".BIN_DIR" ]; then
-   echo ".BIN_DIR not present."
-   echo "Normally it is created by install_piper.sh." 
-   echo "and would contain /path/to/piper/src/python/.venv/bin"
-   exit 1
-else
-    BIN_DIR=$(cat ".BIN_DIR")
-fi    
-
-
-PIPER_BIN="$BIN_DIR/piper"
-# Check if the file 'piper' exists
-if [[ -e "${PIPER_BIN}" ]]; then
-    echo "OK    --  Piper binary found at path: $PIPER_BIN."
-    echo
-    
-else
-    echo "ERROR --  Piper binary not found in $BIN_DIR."
-    echo "          Did you run install_piper.sh?"
-    echo "Exiting."
-    exit 1
-fi
-
-
-if [[ -n "$VIRTUAL_ENV" ]]; then
-    echo
-    echo "OK    --  Virtual environment is active in  $VIRTUAL_ENV"
-    
-elif [ -e "$BIN_DIR/activate" ]; then
-   echo "Activating virtual environment."
-   source $BIN_DIR/activate
-else
-    echo "ERROR --  No python virtual environment was found."
-    echo
-    echo "Exiting."
-    exit 1
-fi
-
-
-
-
 # Use the provided parameter to create the directory name
 VOICE_NAME="$1"
 DIRECTORY="${VOICE_NAME}_dojo"
-
-
-
 
 # Check if the directory already exists
 if [ -d "$DIRECTORY" ]; then
@@ -86,35 +36,17 @@ fi
 # Create the new directory
 mkdir "$DIRECTORY"
 
-# Confirm success
+echo
 echo "Dojo created in : '$PWD/$DIRECTORY'"
 echo "Populating with : '$PWD/DOJO_CONTENTS'"
+
+# the DOJO_CONTENTS directory is copied as the base structure for all new dojos.
 cp -r ./DOJO_CONTENTS/* ./$DIRECTORY
-cd $DIRECTORY
 
-echo "$VOICE_NAME" > .VOICE_NAME
-echo "$BIN_DIR" >  .BIN_DIR
-pwd > .DOJO_DIR
-DOJO_DIR=$(cat .DOJO_DIR)
-
-# If PIPER_PATH is still empty, attempt to infer it from BIN_DIR
-if [ -z "$PIPER_PATH" ]; then
-    # Extract /path/to/piper/
-    PIPER_PATH=$(echo "$BIN_DIR" | grep -oP "^.+?/piper/")
-fi
-
-echo "$PIPER_PATH" >.PIPER_PATH
-cp .PIPER_PATH ./scripts
-cp .BIN_DIR ./scripts
-cp .DOJO_DIR ./scripts
-cp .VOICE_NAME ./scripts
-cp "$initial_dir/.TEXTY_DIR" ./scripts/
 echo
-echo  
 echo -e "  Dojo is ready! You will find it here:  ${CYAN}${DOJO_DIR}${RESET}"
 echo
 echo -e "  use ${YELLOW}run_training.sh${RESET} inside your new dojo to guide you through the training process"
 echo
 echo
 exit 0
-
