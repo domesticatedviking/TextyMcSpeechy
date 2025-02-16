753f756779fa121d38ae822888d5c48e69ad9e6d
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2025-02-15 16:30:35 -0800

    Update run_tensorboard_server.sh

--- tts_dojo/DOJO_CONTENTS/scripts/utils/run_tensorboard_server.sh
+++ tts_dojo/DOJO_CONTENTS/scripts/utils/run_tensorboard_server.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-DOJO_DIR=$(cat .DOJO_DIR)
+# run_tensorboard_server.sh
+# Displays instructions for viewing tensorboard server output and launches server on textymcspeechy-piper container
+# it is assumed that textymcspeechy-piper is already running
+DOJO_NAME=$1
+LIGHTNING_LOGS_PATH=/app/tts_dojo/${DOJO_NAME}/training_folder/lightning_logs
 clear
 echo "   View training progress with Tensorboard"
 echo "   open http://localhost:6006 in your web browser"    
 
-tensorboard --logdir $DOJO_DIR/training_folder/lightning_logs >/dev/null 2>&1 
-read
+docker exec -it textymcspeechy-piper tensorboard --logdir "${LIGHTNING_LOGS_PATH}" --bind_all >/dev/null 2>&1
+read # holds window waiting for input to suppress command prompt
