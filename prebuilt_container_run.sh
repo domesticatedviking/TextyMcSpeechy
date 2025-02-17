8214afd88a5c802d8c2d41969fede9962ee16707
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2025-02-16 21:53:19 -0800

    Update prebuilt_container_run.sh

--- prebuilt_container_run.sh
+++ prebuilt_container_run.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 # prebuilt_container_run.sh  - Launches a prebuilt image of textymcspeechy-piper
 
+fail=0  
+# Check that docker is installd
+if command -v docker &> /dev/null; then
+    :       
+else
+    echo "WARNING -- Required package Docker is not installed."
+    echo "install instructions can be found here:"
+    echo "https://docs.docker.com/engine/install/"
+    echo
+    echo
+    fail=1
+fi
+
+# Check that NVIDIA container toolkit is installed
+if dpkg -l | grep -q nvidia-container-toolkit; then
+    :
+else
+    echo "WARNING! -- Required package NVIDIA Container Toolkit is not installed."
+    echo "install instructions can be found here:"
+    echo "https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html"
+    echo
+    echo
+    fail=1
+fi
+
+if [[ $fail -eq 1 ]]; then
+    echo "Unable to proceed without required tools."
+    echo
+    echo "Press <Enter> to exit"
+    echo
+    exit 1
+ fi
+
+
 # Set default values if not provided
 TMS_USER_ID=${TMS_USER_ID:-1000}
 TMS_GROUP_ID=${TMS_GROUP_ID:-1000}
@@ -17,6 +51,15 @@ echo "          Mounting volume: $TMS_VOLUME_PATH:/app/tts_dojo"
 echo "                    Ports: Exposing 6006 for TensorBoard"
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
+# Check if the Docker image is present
+if ! docker image inspect $IMAGE_NAME > /dev/null 2>&1; then
+    echo "Docker image $IMAGE_NAME not found locally. Pulling image..."
+    docker pull $IMAGE_NAME
+    if [ $? -ne 0 ]; then
+        echo "Failed to pull Docker image $IMAGE_NAME."
+        exit 1
+    fi
+fi
 
 # Run the docker container with CUDA support
 docker run --rm -d \
@@ -41,4 +84,3 @@ else
     echo "Failed to start the container."
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 fi
-
