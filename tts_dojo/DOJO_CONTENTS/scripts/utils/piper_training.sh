33dbcf3b070579a0a81cb9824371ede5b9082a17
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2025-02-20 15:44:45 -0800

    support training models from scratch

--- tts_dojo/DOJO_CONTENTS/scripts/utils/piper_training.sh
+++ tts_dojo/DOJO_CONTENTS/scripts/utils/piper_training.sh
@@ -7,6 +7,8 @@ trap "kill 0" SIGINT
 set +e # Exit immediately if any command returns a non-zero exit code
 
 SETTINGS_FILE="SETTINGS.txt"
+TRAIN_FROM_SCRATCH_FILE="../target_voice_dataset/.SCRATCH"
+
 # infer name of dojo and voice from directory name
 DOJO_NAME=$(basename "$(dirname "$PWD")")  # this script runs from <name>_dojo/scripts so need parent directory   
 VOICE_NAME=$(echo "$DOJO_NAME" | sed 's/_dojo$//')
@@ -28,19 +30,42 @@ else
     exit 1
 fi
 
+
+
+if [[ -f $TRAIN_FROM_SCRATCH_FILE ]]; then
+    TRAIN_FROM_SCRATCH=$(cat $TRAIN_FROM_SCRATCH_FILE)
+else
+    echo "Error: .SCRATCH file not found: $TRAIN_FROM_SCRATCH_FILE ."
+    exit 1 
+fi
+
+
+
 # Check if the starting checkpoint parameter is provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <starting_checkpoint>:path to a checkpoint file"
   exit 1
 fi
-starting_checkpoint=$1
-echo
-echo
-echo "starting_checkpoint path = $starting_checkpoint"
-echo
-echo
 
-# run piper training in docker container (textymcspeechy-piper)
+starting_checkpoint=$1
+
+
+train_from_scratch(){
+docker exec textymcspeechy-piper bash -c "cd /app/piper/src/python \
+    && python -m piper_train \
+    --dataset-dir "/app/tts_dojo/$DOJO_NAME/training_folder/" \
+    --accelerator gpu \
+    --devices 1 \
+    --batch-size $PIPER_BATCH_SIZE\
+    --validation-split 0.0 \
+    --num-test-examples 0 \
+    --max_epochs 30000 \
+    --checkpoint-epochs $PIPER_SAVE_CHECKPOINT_EVERY_N_EPOCHS \
+    --precision 32
+"
+}
+
+train_from_pretrained(){
 docker exec textymcspeechy-piper bash -c "cd /app/piper/src/python \
     && python -m piper_train \
     --dataset-dir "/app/tts_dojo/$DOJO_NAME/training_folder/" \
@@ -54,5 +79,33 @@ docker exec textymcspeechy-piper bash -c "cd /app/piper/src/python \
     --checkpoint-epochs $PIPER_SAVE_CHECKPOINT_EVERY_N_EPOCHS \
     --precision 32
 "
+}
+
+
+if [[ $TRAIN_FROM_SCRATCH == "false" ]]; then
+    echo
+    echo
+    echo "Training from pretrained checkpoint file:"
+    echo "    $starting_checkpoint"
+    echo
+    echo
+else
+    echo
+    echo
+    echo
+    echo "Training model from scratch."
+    echo
+    echo
+fi
+    
+
+
+
+if [ $TRAIN_FROM_SCRATCH == "true" ]; then
+    train_from_scratch
+else
+    train_from_pretrained
+fi
+
 
 exit 0
