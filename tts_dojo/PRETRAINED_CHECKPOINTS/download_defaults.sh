b2df4156ed1cee7e6bbb46e0cb4dd326488ac50e
domesticatedviking <6627466+domesticatedviking@users.noreply.github.com>
2025-03-05 15:04:41 -0800

    rename non-conforming filenames
    Files that do not conform to epoch=xxxxx-step=yyyyy.ckpt naming convention are renamed when downloaded.
    
    Also added summary of available options.

--- tts_dojo/PRETRAINED_CHECKPOINTS/download_defaults.sh
+++ tts_dojo/PRETRAINED_CHECKPOINTS/download_defaults.sh
@@ -14,8 +14,8 @@ PRETRAINED_LANGUAGE_VARFILE="default/.ESPEAK_LANGUAGE"
 
 # Array to store language filenames without extensions
 languages=()
-
-
+conf_file=$1
+available_checkpoints=""
 
 check_language() {
     local language_string="$1"
@@ -115,51 +115,152 @@ get_quality_from_url() {
     echo "$parent_folder"
 }
 
+
+build_conforming_filename() {
+    # Accept the filename as input
+    local original_name="$1"
+
+    # Check if the filename ends with .ckpt, exit if not
+    if [[ ! "$original_name" =~ \.ckpt$ ]]; then
+        echo "Error: Filename must end with .ckpt"
+        return 1
+    fi
+
+    # Extract integers greater than 10 from the filename using regex
+    local integers=$(echo "$original_name" | grep -oE '[0-9]{2,}')
+    
+    # Convert the list of integers to an array
+    IFS=$'\n' read -rd '' -a arr <<< "$integers"
+    
+    # Sort the integers in ascending order
+    IFS=$'\n' sorted_integers=($(sort -n <<<"${arr[*]}"))
+    
+    # Set default values
+    local integer_1=1000
+    local integer_2=11111111
+
+    # Get integer_1 (smallest value) and integer_2 (largest value)
+    if [[ ${#sorted_integers[@]} -gt 0 ]]; then
+        integer_1="${sorted_integers[0]}"
+        if [[ ${#sorted_integers[@]} -gt 1 ]]; then
+            integer_2="${sorted_integers[-1]}"
+        fi
+    fi
+
+    # Output the new filename format
+    echo "epoch=$integer_1-step=$integer_2.ckpt"
+}
+
+
+validate_checkpoint_filename() {
+    local filename="$1"
+    if [[ "$filename" =~ ^epoch=[0-9]+-step=[0-9]+\.ckpt$ ]]; then
+        echo "PASS"
+    else
+        echo "FAIL"
+    fi
+}
+
+
+
+fix_checkpoint_filename() {
+    local filename="$1"
+    local fixed=""
+    local fixed_conforms=""
+    conforms=$(validate_checkpoint_filename $filename)
+    if [[ "$conforms" == "PASS" ]]; then
+        echo "$filename"  # filename provided conforms
+    else
+        fixed=$(build_conforming_filename $filename)
+        fixed_conforms=$(validate_checkpoint_filename $fixed)
+        if [[ "$fixed_conforms" == "PASS" ]]; then
+            echo $fixed
+        else
+            echo "ERROR.  Unable to create a conforming filename from:  $filename."
+            echo "                          attempted fix resulted in:  $fixed" 
+            exit 1
+        fi
+    fi
+
+}
+
 download_urls(){
     local subfolder="$1"  # folder within pretrained folder eg defaults/m_voices
     shift
     local url_array=("${@}") 
     local filename=""
+    local quality=""
+    local url=""
+    local voice_string=""
+    local voice_type=""
     
-    for url in "${url_array[@]}"; do
+    
+    for url in "${url_array[@]}"; do       
         filename=$(get_filename_from_url "$url")
         quality=$(get_quality_from_url "$url")
+        voice_string=$(basename $subfolder) # extract voice folder name   
+        voice_type="${voice_string%_voice}" # remove _voice to get M or F
 
-        if [ -e "$PRETRAINED_TTS_DIR/$subfolder/$quality/$filename" ]; then
-            echo -e "\n$subfolder/$quality/$filename already exists.  Not downloading."
+        if [[ -n $filename ]] && [[ -n $url ]]; then
+            validname=$(fix_checkpoint_filename "$filename")
+            echo
+            echo "***********************************************************************"
+            echo " A URL for ${voice_type}_voice, $quality quality was supplied by $conf_file.conf "
+            echo
+
+            if [ -e "$PRETRAINED_TTS_DIR/$subfolder/$quality/$validname" ]; then
+                echo -e "\n$subfolder/$quality/$validname already exists.  Not downloading."
+                echo
+                echo
+                available_checkpoints+="            ${voice_type} - ${quality} quality"$'\n'   # build summary of available combinations 
+            else
+                echo -e "Attempting to download from source:"
+                echo -e "    $url"
+                echo -e "destination: $subfolder/$quality/$validname."
+
+                if wget -O "$PRETRAINED_TTS_DIR/$subfolder/$quality/$validname" "$url"; then
+                    available_checkpoints+="            ${voice_type} - ${quality} quality"$'\n' # build summary of available combinations
+                else
+                    echo "Download failed: $url" >&2
+                    available_checkpoints+="            ${voice_type} - ${quality} quality --- DOWNLOAD FAILED"$'\n' # build summary of available combinations
+                fi
+            fi
         else
-            echo -e "\nDownloading $filename to $subfolder/$quality."
-            wget -O "$PRETRAINED_TTS_DIR/$subfolder/$quality/$filename"  $url
+            if [[ -n $url ]]; then
+                echo "Url provided was: $url"
+                echo "No download available for voice type $(basename $subfolder) at quality level $quality"
+            fi
+
         fi
     done
 }
-clear
-echo "Warning: At present this script provides only partial assistance downloading pretrained checkpoint files."
-echo "Some of the recently added .conf files include filenames that do not fit previous naming conventions."
-echo "textymcspeechy currently requires .ckpt files to be named according to the following pattern:"
-echo "eg:     epoch=1234-step=1234567890.ckpt"
-echo "A fix will be coming soon, but in the meantime you will need to rename any files that do not comply."
-echo "Also keep in mind that pretrained checkpoints are not necessarily available for all voice types and quality levels"
-echo "Please check PRETRAINED_CHECKPOINTS/default subfolders for a properly named .ckpt file before training your model"
-echo
-echo "Press <Enter>"
-read
 
 download_urls "default/M_voice" "${M_URLS[@]}"
 download_urls "default/F_voice" "${F_URLS[@]}"
 
 if [ ! -z $ESPEAK_LANGUAGE ]; then
-    echo "Configured espeak language: $ESPEAK_LANGUAGE "
     echo $ESPEAK_LANGUAGE > $PRETRAINED_LANGUAGE_VARFILE 
 else
     echo "Warning: no value for ESPEAK_LANGUAGE provided by '$requested_language.conf'"
 fi
+echo
+echo
+echo
+echo
+echo "SUMMARY OF AVAILABLE PRETRAINED CHECKPOINTS - PLEASE READ"
+echo
+echo "    $conf_file.conf included pretrained checkpoints for the following combinations of voice type and quality level:"
+echo
+echo -n "$available_checkpoints"
+echo
 
+
+echo "    If the combination of voice type and quality level you wish to use is not listed, your options are to:"
+echo "        - train from scratch"
+echo "        - copy any properly named .ckpt file to the correct subfolder matching its voice type and quality level within PRETRAINED_CHECKPOINTS/default"
+echo "        - the .ckpt file must be named using this pattern (any name in this pattern will work, pattern is case sensitive):"
+echo "                          epoch=1000-step=11111111111.ckpt"
+echo "        - you can download a .ckpt from another language at https://huggingface.co/datasets/rhasspy/piper-checkpoints/tree/main"
+echo "        - you can also use any the .ckpt files generated by your own training sessions (saved in your voice dojo's voice_checkpoints folder)"  
 echo
-echo "Since pretrained checkpoints might not be available for all quality levels and voice types,"
-echo "  please check the subfolders of PRETRAINED_CHECKPOINTS/default  "
-echo "  to ensure that a .ckpt file is available for your desired voice type and quality level before you start training."
-echo "If no .ckpt file is available, you can either train from scratch, or copy a .ckpt from a different language into the relevant folder."
-echo "You can also use .ckpt files generated during your own training sessions (found in dojo's voice_checkpoints folder) as pretrained checkpoints."
-echo
-echo "A collection of pretrained checkpoints can be found at https://huggingface.co/datasets/rhasspy/piper-checkpoints/tree/main"
+echo "Exiting."
